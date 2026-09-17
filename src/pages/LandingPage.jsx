d851307eb6f31e25c6b2289150146774de5ff539
JihanN-arch <jnpermata07@gmail.com>
2026-09-17 23:25:58 +0700

    initial commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+function LandingPage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Landing Page</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPage from "../pages/LandingPage";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Landing Page */}
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
